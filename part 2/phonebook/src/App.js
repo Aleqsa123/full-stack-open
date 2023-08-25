f36e16c46446e00679b432e1b38031236d420778
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-08-25 21:03:20 +0400

    Phonebook app initial folders and files added, exercise 1 added

---
+++ part 2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPersons = [ {name: newName} ]
+  
+    setPersons(persons.concat(newPersons))
+    setNewName('')
+  }
+
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+            />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      { persons.map((person, i)=> {return <p key={i}>{person.name}</p>})}
+    </div>
+  )
+}
+
+export default App

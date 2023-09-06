d9ea533f3bc5398aaf5c5f586ebbb6893585640a
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-09-06 21:48:52 +0400

    finish 2.18* Data for countries, step1, country app feature created, data fetched

--- part 2/countries/src/App.js
+++ part 2/countries/src/App.js
@@ -4,8 +4,6 @@ import getCountries from './services/countries';
 const App = () => {
   const [countries, setCountries] = useState([]);
 
-  const [name, setName] = useState('new name...'); 
-
   useEffect(() => {
     getCountries
       .getAll()
@@ -14,28 +12,71 @@ const App = () => {
       })
   }, [])
 
-
-  const handleChange = (event) => {
-    setName(event.target.value)
+  const [newSearch, setNewSearch] = useState('')  
+  
+  //Takes value from searchbar and sets showAll's state to false 
+  const handleSearch = (event) => {
+    setNewSearch(event.target.value.toLowerCase());
+    if (newSearch !== ''){return setShowAll(false)}
   }
 
-  return (
-    <div>
-      <div>
+  const [showAll, setShowAll] = useState(true)
+
+  //Show only persons from searchbar
+  const countryToShow = showAll
+    ? countries
+    : countries.filter((country) => country.name.common.toLowerCase().includes(newSearch));
+
+    if (countryToShow.length > 10){
+      return (<div>
+                <form>
+                  find countries: <input 
+                                    value={newSearch} 
+                                    onChange={handleSearch} 
+                                  />
+                </form>
+                <p>Too many matches, specify another filter</p>
+              </div>)
+    } else if (countryToShow.length <= 10 && countryToShow.length > 1){
+      return (
+        <div>
+          <div>
+            <form>
+              find countries: <input 
+                                value={newSearch} 
+                                onChange={handleSearch} 
+                               />
+            </form>
+          </div>
+          <ol>
+          {countryToShow.map(country =>
+              <li key={country.cca2}> {country.name.common} </li>
+            )}
+          </ol>
+        </div>
+      )
+     }else if (countryToShow.length == 1){
+      return (<div>
         <form>
           find countries: <input 
-                            value={name} 
-                            onChange={handleChange} 
-                           />
+                            value={newSearch} 
+                            onChange={handleSearch} 
+                          />
         </form>
-      </div>
-      <ol>
-      {countries.map(country =>
-          <li key={country.cca2}> {country.name.common} </li>
-        )}
-      </ol>
-    </div>
-  )
-}
+              <h1>{countryToShow[0].name.common} </h1> 
+              <p> capital {countryToShow[0].capital[0]} </p>
+              <p> area {countryToShow[0].area} </p>
+        <div>languages: 
+
+          <ul>
+            {Object.values(countryToShow[0].languages).map( (l, index) => { return <li key = {index}> {l} </li> })}
+          </ul>
+        </div>
+        <img src={countryToShow[0].flags.png} alt = {countryToShow[0].flags.alt} />
+
+      </div>)
+    }
+    } 
+  
 
 export default App 
